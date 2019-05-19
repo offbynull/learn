8f33370911756d3e3d6dce3245de4df6d3d68bc0
offbynull <offbynull@gmail.com>
2019-05-19 16:07:54 -0400

    Ignore build files and read MD from file

---
+++ markdown/input.md
@@ -0,0 +1,30 @@
+# heading1
+```{toc}
+```
+`{bookmark} my message` this is inline
+## heading2
+### heading3
+# heading4
+## heading5
+<b>yo</b>
+[this my link](http://github.com) jump to my message.
+however, this `{bookmark-ref-ignore} my message` should not be highlighted
+link using `{bm} shorthand` can be accessed using shorthand but not `{bm-ri} shorthand`
+
+Here's a dot graph
+
+```{dot}
+digraph { a -> b; }
+```
+
+```{note}
+This is a custom note
+```
+
+This is an inline mathjax expression: `{mj} \frac{a}{b}`
+
+This is a block mathjax expression:
+
+```{mj}
+\frac{c}{d}
+```
--- markdown/src/index.ts
+++ markdown/src/index.ts
@@ -2,41 +2,9 @@ import Markdown from './markdown/markdown';
 import * as WebResourceInliner from 'web-resource-inliner';
 import * as FileSystem from 'fs';
 
-const mdInstantance: Markdown = new Markdown();
-const result = mdInstantance.render(`
-# heading1
-\`\`\`{toc}
-\`\`\`
-\`{bookmark} my message\` this is inline
-## heading2
-### heading3
-# heading4
-## heading5
-<b>yo</b>
-[this my link](http://github.com) jump to my message.
-however, this \`{bookmark-ref-ignore} my message\` should not be highlighted
-link using \`{bm} shorthand\` can be accessed using shorthand but not \`{bm-ri} shorthand\`
+const input = FileSystem.readFileSync('input.md', 'utf-8').toString();
 
-Here's a dot graph
-
-\`\`\`{dot}
-digraph { a -> b; }
-\`\`\`
-
-\`\`\`{note}
-This is a custom note
-\`\`\`
-
-This is an inline mathjax expression: \`{mj} \\frac{a}{b}\`
-
-This is a block mathjax expression:
-
-\`\`\`{mj}
-\\frac{c}{d}
-\`\`\`
-
-`);
-
+const result = new Markdown().render(input);
 
 const config: WebResourceInliner.Options = {
     'fileContent': result,
@@ -46,11 +14,13 @@ const config: WebResourceInliner.Options = {
     'svgs': true,
     'strict': true
 };
-WebResourceInliner.html(config, (error: any, result: any) => {
-    if (error) {
-        console.error(error);
-        return;
+WebResourceInliner.html(
+    config,
+    (error, result) => {
+        if (error) {
+            console.error(error);
+            return;
+        }
+        FileSystem.writeFileSync('output.html', result as string);
     }
-
-    FileSystem.writeFile('output.html', result as string, err => { if (err) console.error(err) });
-});
+);
