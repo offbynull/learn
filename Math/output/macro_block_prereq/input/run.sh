902bee0e1be9f541f9444c7b710d6435c0d6d5c1
offbynull <offbynull@gmail.com>
2020-04-11 15:29:29 -0400

    Using macros for old baked in extensions

--- Math/output/macro_block_prereq/input/run.sh
+++ Math/output/macro_block_prereq/input/run.sh
@@ -1,6 +1,1 @@
-rm -rf /tmp/proj
-mkdir /tmp/proj
-cp -r /input/prereq_code/* /tmp/proj/
-cd /tmp/proj
-cat /input/input.data
-python PreReq.py < /input/input.data > /output/output.md
+python /input/Main.py < /input/input.data > /output/output.md
